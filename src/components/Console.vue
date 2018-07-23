baef8e017981e7584e4c239b71b9178821fdf075
Eduardo Aguad <eaguad@meat.cl>
2018-07-23 09:02:59 -0400

    Resize delimiter

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -5,7 +5,7 @@
       <p>Welcome to my humble webpage ;)</p>
       <p>I'm Eduardo Aguad and I live in Santiago, Chile. Doing backend is my favorite thing to do but I worked in some cool apps with javascript and vue.js framework.</p>
       <p>Also, I enjoy working with linux servers (specially debian-based). Some of the services I can configure are nginx, mysql, php-fpm, ssh, etc. Even this is not my "primary skill", I know to look when I need to add a feature or search for an error.</p>
-      <p>---------------------------------------------</p>
+      <p>---------------------------------------</p>
       <p>I am self-taught and consider myself a very lucky because I have worked with amazing people.</p>
       <p>Programing Skills:</p>
       <ul>
